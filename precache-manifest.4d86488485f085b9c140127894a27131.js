8a424578d259668aece0038f348b1678654d3d47
Lee Trout <lee@leetrout.com>
2019-04-23 23:15:00 -0400

    Add initial site files

---
+++ precache-manifest.4d86488485f085b9c140127894a27131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2421b4f2710e8368e136",
+    "url": "/hospital-pricing/static/js/runtime~main.8323c5ab.js"
+  },
+  {
+    "revision": "75d54d0cd90bec81ad81",
+    "url": "/hospital-pricing/static/js/main.5cc26ff1.chunk.js"
+  },
+  {
+    "revision": "3910e773f50de45c431f",
+    "url": "/hospital-pricing/static/js/2.64791022.chunk.js"
+  },
+  {
+    "revision": "75d54d0cd90bec81ad81",
+    "url": "/hospital-pricing/static/css/main.b10f03eb.chunk.css"
+  },
+  {
+    "revision": "3910e773f50de45c431f",
+    "url": "/hospital-pricing/static/css/2.b361e481.chunk.css"
+  },
+  {
+    "revision": "b72297f87d9b4bc9e3a93738f0a6acc1",
+    "url": "/hospital-pricing/index.html"
+  }
+];
